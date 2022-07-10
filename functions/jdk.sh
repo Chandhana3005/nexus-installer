f0283c1e0f50815d6cb7210635d95e36a1ea8190
ChandanaB99 <chanducalm.99@gmail.com>
2022-07-10 12:35:08 -0400

    jdk

--- functions/jdk.sh
+++ functions/jdk.sh
@@ -1,7 +1,13 @@
 installJdk8() {
-  sudo apt-get install -y python-software-properties debconf-utils
-  sudo add-apt-repository -y ppa:webupd8team/java
-  sudo apt-get update
-  echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-  sudo apt-get install -y oracle-java8-installer
+  # sudo apt-get install -y python-software-properties debconf-utils
+  # sudo add-apt-repository -y ppa:webupd8team/java
+  # sudo apt-get update
+  # echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+  # sudo apt-get install -y oracle-java8-installer
+if java -version 2>&1 >/dev/null | grep -q "java version" ; then
+  echo "Java installing."
+  sudo yum install -y java-11-openjdk-devel
+else
+  echo "Java NOT installed!"
+fi
 }
